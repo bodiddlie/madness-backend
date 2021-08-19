175f13780a8bcc447010c62644fc3fd4988b5c82
bodiddlie <bodiddlie@gmail.com>
2021-08-18 17:08:56 -0700

    add sort param to gb call

--- game/search.js
+++ game/search.js
@@ -7,7 +7,7 @@ const gbUrl = process.env.GB_URL;
 const apiKey = process.env.GB_API_KEY;
 
 module.exports.search = auth()(async (event) => {
-  const url = `${gbUrl}?api_key=${apiKey}&format=json&filter=name:${event.queryStringParameters.name}`;
+  const url = `${gbUrl}?api_key=${apiKey}&format=json&filter=name:${event.queryStringParameters.name}&sort=name:asc`;
 
   try {
     const result = await axios.get(url);
