c7d807da10089a688680c8aa55d2de2b4abb33c1
bodiddlie <bodiddlie@gmail.com>
2021-08-23 21:00:47 -0700

    add complete endpoint

---
+++ game/complete.js
@@ -0,0 +1,28 @@
+const dynamodb = require('../libs/dynamo');
+const { success, failure } = require('../libs/response');
+const auth = require('../libs/auth');
+
+module.exports.complete = auth()(async (event) => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      PK: `USER#${event.userEmail}`,
+      SK: `GAME#${event.pathParameters.id}`,
+    },
+    UpdateExpression: 'SET completed = :completed',
+    ExpressionAttributeValues: {
+      ':completed': true,
+    },
+    ReturnvValues: 'ALL_NEW',
+  };
+
+  try {
+    await dynamodb.call('update', params);
+    return success();
+  } catch (err) {
+    console.error(
+      `Failure while completing game ${event.pathParameters.id}: ${err}`
+    );
+    return failure({ message: 'Error occurred while completing game.' });
+  }
+});
